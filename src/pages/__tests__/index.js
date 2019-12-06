e0289f90aafb827bb1f1e6c9e5e8be1006153f75
Angelus, Corey <corey.angelus@accenture.com>
2019-12-05 21:55:49 -0500

    remove typo

--- src/pages/__tests__/index.js
+++ src/pages/__tests__/index.js
@@ -13,7 +13,7 @@ describe("Index Component", () => {
   beforeEach(() => {
     wrapper = mount(
       <BrowserRouter>
-        <Index />{" "}
+        <Index />
       </BrowserRouter>
     )
   })
