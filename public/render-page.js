e601ee2f1430cf4b2cdff3d32b86c0ab97b1c66b
AngelusProductons <angelusproductions@gmail.com>
2023-06-21 00:41:49 -0700

    change favicon

--- public/render-page.js
+++ public/render-page.js
@@ -118,13 +118,13 @@ var plugins = [{
     "background_color": "#663399",
     "theme_color": "#663399",
     "display": "minimal-ui",
-    "icon": "src/images/gatsby-icon.png",
+    "icon": "src/images/favicon-16x16.png",
     "legacy": true,
     "theme_color_in_head": true,
     "cache_busting_mode": "query",
     "crossOrigin": "anonymous",
     "include_favicon": true,
-    "cacheDigest": "4a9773549091c227cd2eb82ccd9c5e3a"
+    "cacheDigest": "8cc9c6feebf1bdd79f4e1cfa9f2caafa"
   }
 }];
 // During bootstrap, we write requires at top of this file which looks like:
